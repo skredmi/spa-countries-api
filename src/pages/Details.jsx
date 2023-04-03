eb1863f44b5f6b4f99acda4f45c2bac175400159
Dmitry Skrebkov <skredmi91@gmail.com>
2023-04-03 19:07:37 +0300

    deploy

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -3,21 +3,26 @@ import { useParams, useNavigate } from "react-router-dom";
 import { IoArrowBack } from "react-icons/io5";
 import { useState, useEffect } from "react";
 import { searchByCountry } from "../config";
+import { Button } from "../components/Button";
+import { CardDetail } from "../components/CardDetail";
 
 export const Details = () => {
   const { name } = useParams();
   const navigate = useNavigate();
 
   const [country, setCountry] = useState(null);
-
+  console.log(country);
   useEffect(() => {
     axios.get(searchByCountry(name)).then(({ data }) => setCountry(data[0]));
   }, [name]);
 
   return (
     <div>
-      <button onClick={() => navigate(-1)}>Go Back</button>
-      Details {name}
+      <Button onClick={() => navigate(-1)}>
+        <IoArrowBack />
+        Back
+      </Button>
+      {country && <CardDetail {...country} navigate={navigate} />}
     </div>
   );
 };
